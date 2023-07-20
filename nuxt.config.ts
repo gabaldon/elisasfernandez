89b09c8cbbac81cbea7d16f48a87c971dbf08ebf
gabaldon <maria@otherplane.com>
2023-07-20 19:42:22 +0200

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,72 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: false,
+  app: {
+    baseURL: '/',
+    head: {
+      title: 'Elisa S Fernández',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        {
+          hid: 'title',
+          name: 'title',
+          content: 'Elisa S Fernández',
+        },
+        { hid: 'description', name: 'description', content: process.env.npm_package_description || 'Elisa S Fernández is a Photographer based in Madrid - elisasanchezfernandez@gmail.com' },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: 'Elisa S Fernández',
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content:
+            'Elisa S Fernández is a Photographer based in Madrid - elisasanchezfernandez@gmail.com',
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: '/logo.png',
+        },
+        {
+          hid: 'twitter:image:alt',
+          name: 'twitter:image:alt',
+          content: 'Elisa S Fernández',
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: 'Elisa S Fernández',
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content:
+            'Elisa S Fernández is a Photographer based in Madrid - elisasanchezfernandez@gmail.com',
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: '/logo.png',
+        },
+        {
+          hid: 'og:image:secure_url',
+          property: 'og:image:secure_url',
+          content: '/logo.png',
+        },
+        {
+          hid: 'og:image:alt',
+          property: 'og:image:alt',
+          content: 'Elisa S Fernández',
+        },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap' }
+      ]
+    },
+  },
+  components: true,
+})
---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <h1>
+      Comming soon...
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      name: 'index',
+      title: this.$t('head.title'),
+      meta: [
+        {
+          name: this.$t('head.title'),
+          content: this.$t('description'),
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
