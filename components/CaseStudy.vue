dde13ac2a4552d16e0e0f80a3e6d0f137a23a0fb
gabaldon <maria@otherplane.com>
2023-07-27 13:33:30 +0200

    feat: implement compo for case study

---
+++ components/CaseStudy.vue
@@ -0,0 +1,22 @@
+<template>
+  <object class="pdf" :data="pdfUrl" type="application/pdf">
+    <p>
+      Your web browser doesn't have a PDF plugin. Instead you can
+      <a :href="pdfUrl"> click here to download the PDF file. </a>
+    </p>
+  </object>
+</template>
+<script setup lang="ts">
+defineProps({
+  pdfUrl: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+<style lang="scss">
+.pdf {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
--- pages/blablacar/index.vue
+++ pages/blablacar/index.vue
@@ -1,20 +1,5 @@
 <template>
   <NuxtLayout>
-    <object
-      class="pdf"
-      data="/pdf/blablacar-case-study.pdf"
-      type="application/pdf"
-    >
-      <p>
-        Your web browser doesn't have a PDF plugin. Instead you can
-        <a href="filename.pdf">click here to download the PDF file.</a>
-      </p>
-    </object>
+    <CaseStudy :pdf-url="'/pdf/blablacar-case-study.pdf'" />
   </NuxtLayout>
 </template>
-<style lang="scss">
-.pdf {
-  width: 100vw;
-  height: 100vh;
-}
-</style>
